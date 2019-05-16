051ddb98584fc8e11fcee71f0570f2f34a35f035
BUI Duc Trieu <buiductrieu1006@gmail.com>
2019-05-16 09:33:34 +0700

    first commit

---
+++ src/Components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import classes from './NavigationItems.module.css';
+import NavigationItem from './NavigationItem/NavigationItem';
+
+const navigationItems = () => (
+    <ul className={classes.NavigationItems}>
+        <NavigationItem link="/" exact>Home</NavigationItem>
+        <NavigationItem link="/login" >Login</NavigationItem>
+    </ul>
+);
+
+export default navigationItems;
---
+++ src/Containers/Layout/Layout.js
@@ -0,0 +1,39 @@
+import React, {Fragment, Component} from 'react';
+
+import classes from './Layout.module.css'; 
+import Toolbar from '../../Components/Navigation/Toolbar/Toolbar';
+import SideDrawer from '../../Components/Navigation/SideDrawer/SideDrawer';
+
+class Layout extends Component {
+    state = {
+        showSideDrawer: false
+    }
+
+    sideDrawerCloseHandler = () => {
+        this.setState({showSideDrawer: false});
+    }
+
+    sideDrawerToggleHandler = () => {
+        this.setState((prevState) => {
+            return {showSideDrawer: !prevState.showSideDrawer}
+        }); 
+    }
+
+    render () {
+        return (
+            <Fragment>
+                <Toolbar 
+                    drawerToggleClicked={this.sideDrawerToggleHandler} />
+                <SideDrawer
+                    open={this.state.showSideDrawer} 
+                    closed={this.sideDrawerCloseHandler} />
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Fragment>
+        )
+    }
+}
+
+
+export default Layout;
